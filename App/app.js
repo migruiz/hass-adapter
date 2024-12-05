5e350e1b4337e553961db6ca184b77920fbbd9bf
Miguel <mig.ruiz@gmail.com>
2024-12-05 19:10:42 +0000

    resetting onchange

--- App/app.js
+++ App/app.js
@@ -92,19 +92,28 @@ const scaleReadings = sharedReadings.pipe(
         const stableReadings = newHistory.length === HISTORY_NUMBER
         if (!stableReadings) {
             return {
-                history: newHistory
+                history: newHistory,
+                catState: acc.catState
             }
         }
         const averageKg = getAverageKg(newHistory)
         const estimatedAction = estimateAction({ currentKg: curr.valuekg, averageKg })
         const catState = getCatState({ estimatedAction, prevCatState: acc.catState, averageKg })
-        return {
-            ...curr,
-            processMessage: true,
-            history: newHistory,
-            averageKg,
-            estimatedAction,
-            catState
+        if (acc.catState && catState !== acc.catState) {
+            return {
+                history: [],
+                catState: acc.catState
+            }
+        }
+        else {
+            return {
+                ...curr,
+                processMessage: true,
+                history: newHistory,
+                averageKg,
+                estimatedAction,
+                catState
+            }
         }
     }, { history: [] }),
     filter(r => r.processMessage)
